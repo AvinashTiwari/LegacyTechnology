39822b4869a539871e3bdc07ba78df6a03b181d0
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-20 17:55:44 -0600

    no message

---
+++ RapidCPlusPlus/2_Radius_Circle/CircleExcersise/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+double PI, radius;
+double area;
+
+cout << "Please enter value of PI : ";
+cin  >> PI;
+
+cout << "Please enter value of radius : ";
+cin  >> radius;
+
+area = PI * radius * radius;
+
+cout << "Area of Circle is = " << area << endl;
+
+
+
+return 0;
+}
