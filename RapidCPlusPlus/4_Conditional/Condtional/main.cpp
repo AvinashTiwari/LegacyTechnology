256be178b0205ec30a99b54b91cd922d62893b72
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-24 16:35:20 -0600

    no message

---
+++ RapidCPlusPlus/4_Conditional/Condtional/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10, b = -10;
+
+    if(a < 10){
+        cout << "a is smaller then b";
+    }
+    else if (a==10){
+        cout << "a is 10";
+
+    }
+    else
+    {
+        cout << " a is bigger then b";
+    }
+
+    return 0;
+}
